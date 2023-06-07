9a4383232aeac93d1a1d525389c9604ba4c3d9f4
Mitanshk01 <97786599+Mitanshk01@users.noreply.github.com>
2023-06-07 20:04:44 +0530

    Add files via upload

---
+++ user/proc.d
@@ -0,0 +1,3 @@
+kernel/proc.o: kernel/proc.c kernel/types.h kernel/param.h \
+ kernel/memlayout.h kernel/riscv.h kernel/spinlock.h kernel/proc.h \
+ kernel/defs.h /usr/lib/gcc/riscv64-unknown-elf/9.3.0/include/stddef.h
